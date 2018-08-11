19706f8463775c7e822dc811618c781ca8765000
Sarah Addo <sarahaddo@Sarahs-MBP.fios-router.home>
2018-08-11 12:20:47 -0400

    add format and close button(no functionality)

--- script.js
+++ script.js
@@ -9,11 +9,17 @@ function addList() {
 
 
 	let ul = document.getElementById("list");
-	let newItem = document.getElementById("newTodo").value;
+	let newItem = document.getElementById("inputArea").value;
 	let li = document.createElement('li');
+	let button = document.createElement("button");
+	button.innerHTML = "\u00d7";
+
+	button.className = "close";
+
 	li.appendChild(document.createTextNode(newItem));
+	li.appendChild(button);
 	ul.appendChild(li);
 
-	document.getElementById("newTodo").value = '';
+	document.getElementById("inputArea").value = '';
 
 }
