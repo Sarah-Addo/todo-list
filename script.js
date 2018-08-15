89b872c23a92b074f9ca355085dcdd9207c0728f
Sarah Addo <sarahaddo@Sarahs-MBP.fios-router.home>
2018-08-15 16:06:02 -0400

    add readablity to code, change how new list item is created and how its contents are ordered and styled

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 
-//Allows user to enter a new list item with the enter keu
+//Allows user to enter a new list item with the enter key
 function handleKeyPress(e) {
 	if(e.keyCode == 13) {
 		addList();
 	}
 }
 
-//Adds element to the todo list
+//creates a list item and adds it to the unordered list
 function addList() {
 
-	//creates a list item and adds it to the unordered list
+	//gets the unordered list and creates a new li called listItem
 	let ul = document.getElementById("list");
-	let newItem = document.getElementById("inputArea").value;
 	let li = document.createElement('li');
-
-	li.appendChild(document.createTextNode(newItem));
 	li.className ="listItem";
-	ul.appendChild(li);
 
-	document.getElementById("inputArea").value = '';
+	//creates a p called listText to go inside listItem and 
+	//the text newItem was the userInput
+	let p = document.createElement("p");
+	let newItem = document.getElementById("inputArea").value;
+	p.appendChild(document.createTextNode(newItem));
+	p.className = "listText";
+	li.appendChild(p);
+	
 
 	//creates a span element with the close symbol and adds it to the end
 	//of each list item
-	let myNodeList = document.getElementsByClassName("listItem");
-	
 	let span = document.createElement("span");
-		let txt = document.createTextNode("\u00d7");
+	let txt = document.createTextNode("\u00d7");
+	span.className = "close";
+	span.appendChild(txt); 
+	li.appendChild(span);
 
-		span.className = "close";
-		span.appendChild(txt);
+	//adds a li containing the p and span element to the ul list
+	ul.appendChild(li);
 
-	for(let i = 0; i < myNodeList.length; i++) {
-		
-		myNodeList[i].appendChild(span);
-
-
-	}
+	//resets the input field
+	document.getElementById("inputArea").value = '';
 
 }
